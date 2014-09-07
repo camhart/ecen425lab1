23b2773facf5e3e9614d2688910803245f9ebaa0
camhart <camhart73@gmail.com>
2014-09-07 17:23:29 -0600

    cleaned up, 19 instructions

--- lab1asm.s
+++ lab1asm.s
@@ -6,39 +6,23 @@
 	align	2
 
 AsmFunction:  
-	;push bx
-	;push cx
-	;push dx
         push bp
         mov bp, sp   
-        ; end function setup    
  
 	push bx
 	push cx
 	push dx
 	mov	al, byte [bp+6]	;ax = b
 	add	al, byte [bp+8]	;ax = b + c
-	cbw
-	mov	bx, [bp+4]	;bx = a
+	cbw			;sign extend al to fill ax
 	
-	imul	bx
-
-	;a*(b+c)
+	imul	word [bp+4]
 
 	mov	cx, [bp+10]
 	sub	cx, [bp+12]	;
 
-	;(d-e)=dx
-
 	idiv cx
 	add ax, [gvar]
-	;;mov ax, [gvar]
-;	;mov ax, dx
-;	;mov	ax, [bp+4] ;arg1 int a
-;	;mov     ax, [bp+6] ;arg2 char b
-;	;mov     ax, [bp+8] ;arg3 char c
-;	;mov     ax, [bp+10] ;arg4 int d
-;	;mov     ax, [bp+12] ;arg5 int e
 	
 	pop dx
 	pop cx
@@ -46,8 +30,5 @@ AsmFunction:
 	; function tear down (ax ret register)
 	mov sp, bp
         pop bp
-	;pop dx
-	;pop cx
-	;pop bx
 	ret
 
